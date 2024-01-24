6e39ca107d1d15d3721c1617dc41e8b17cc9ca29
Rishav-R03 <rishav042023@gmail.com>
2024-01-24 22:56:01 +0530

    Numpy Practice

---
+++ NumPy/Problem1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import time
+import sys
+#show the version and the configuration of numpy
+print(np.__version__)
+print(np.show_config)
+
+# numpy add function 
+print(np.info(np.add))
+
+# numpy program to test whether array elements are zaero
+
