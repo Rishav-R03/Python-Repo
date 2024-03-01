3e4a070927bffa8de74092ed9d427ddf060e3eff
Rishav-R03 <rishav042023@gmail.com>
2024-03-01 21:48:53 +0530

    Numpy practice

---
+++ NumPy/addFunction.py
@@ -0,0 +1,6 @@
+import numpy as np
+print(np.info(np.add)) # prints information about the np.add func
+# this function is used for element-wise addition of two arrays
+
+# np.info provides detailed information about a given NumPy func
+
