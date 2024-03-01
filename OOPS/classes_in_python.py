d7b993debf911bec11e39be0b1fc6308b58ba85f
Rishav-R03 <rishav042023@gmail.com>
2024-03-01 21:56:18 +0530

    Classes in python

---
+++ OOPS/classes_in_python.py
@@ -0,0 +1,27 @@
+# item1 = "Phone"
+# item_price = 10
+# item_quantity = 10
+# item_price_total = 100
+
+# print(type(item1))
+# print(type(item_price))
+# print(type(item_quantity))
+# print(type(item_price_total))
+
+class Item: # creating class
+    # pass
+    def cal_total():
+        pass
+
+item1 = Item()
+# creating attributes
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 50
+
+item2 = Item()
+# creating attributes
+item1.name = "Laptop"
+item1.price = 1000
+item1.quantity = 10
+print(type(item1))
